047509a454ce788e641ebfa614c0fd9d1b3d1dfa
Oleh Lytovchenko <lytowchenko@gmail.com>
2015-08-13 17:50:14 +0300

    ticket:1 add Person model test

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    jabber = models.CharField(max_length=50)
+    skype = models.CharField(max_length=50)
+    site = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "Person"
+        verbose_name_plural = "Persons"
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
