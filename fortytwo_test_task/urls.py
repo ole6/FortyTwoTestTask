fc00733a5e2ad09af129e471aea8b89997ae4384
Oleh Lytovchenko <lytowchenko@gmail.com>
2015-08-13 16:42:15 +0300

    ticket:1 home template test pass

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
+from apps.hello.views import home
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', home, name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
